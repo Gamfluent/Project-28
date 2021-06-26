75e6c69e33c5a8e054a5c041db46e5f1f814e105
Gamfluent <79038485+Gamfluent@users.noreply.github.com>
2021-06-26 21:31:57 +1200

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,36 @@
+class Launcher {
+    constructor(body, anchor){
+        this.bodyA = body;
+        this.pointB = anchor;
+
+        var options = {
+            bodyA : body,
+            pointB : anchor,
+            stiffness : 0.01,
+            length : 1,
+        }
+
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+
+            strokeWeight(4);
+            stroke("red");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);   
+        }
+    }
+}
